8874a1d8399dad82f0d9e4885dd06a3502faf0be
gastometrobr-svg <gastometrobr@gmail.com>
2026-08-07 20:48:29 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,23 +1,15 @@
-const CACHE_NAME = 'gastometro-v2';
+const CACHE_NAME = 'gastometro-v3';
 const urlsToCache = [
   '/gastometro/',
-  '/gastometro/index.html'
+  '/gastometro/index.html',
+  '/gastometro/manifest.json'
 ];
 
 self.addEventListener('install', event => {
-  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      if (response) return response;
-      return fetch(event.request);
-    })
-  );
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
@@ -25,5 +17,12 @@ self.addEventListener('activate', event => {
     caches.keys().then(keys =>
       Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
     )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
